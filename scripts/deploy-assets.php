8a5494ac4cd4e7fe086242777695bf4eabb00d76
Jeremy Jones <jkjones3@uic.edu>
2025-11-19 14:46:11 -0600

    MNTC-1648 #time 5m [dev] re add script to deploy assets

---
+++ scripts/deploy-assets.php
@@ -0,0 +1,28 @@
+#!/usr/bin/env php
+<?php
+
+try {
+    if (!isset($argv[1])) {
+        throw new Exception('Asset path argument required');
+    }
+
+    $assetPath = trim($argv[1]);
+
+    if (substr($assetPath, -1) == '/') {
+        $assetPath = rtrim($assetPath, '/');
+    }
+
+    if (!is_writable($assetPath)) {
+        throw new Exception("Asset path is not writable");
+    }
+
+    $dir = new DirectoryIterator(dirname(__DIR__) . '/../src/Shibboleth/assets');
+    foreach ($dir as $file) {
+        if (!$file->isDot()) {
+            copy($file->getRealPath(), $assetPath . '/' . $file->getFilename());
+        }
+    }
+} catch (Exception $e) {
+    echo $e->getMessage() . PHP_EOL;
+}
+
